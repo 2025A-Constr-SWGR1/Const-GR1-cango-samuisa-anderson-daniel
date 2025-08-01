f4d0e7826f8a82d3742e3299eb0144ad437e0537
AndersonCango <118143904+AndersonCango@users.noreply.github.com>
2025-07-31 20:50:08 -0500

    feat: add own library

--- librerias/packages/index.js
+++ librerias/packages/index.js
@@ -1,15 +1,15 @@
 const paqueteSuma = require("2025a-swgr1-adcs-suma");
-const respuestaSuma = paqueteSuma.suma(4, 9);
-console.log("La respuesta de la suma es: ", respuestaSuma);
+const respuestaSuma = paqueteSuma.suma(2, 1);
+console.log("La respuesta de la suma es: ", respuestaSuma); // 3
 
 const paqueteResta = require("2025a-swgr1-adcs-resta");
-const respuestaResta = paqueteResta.resta(24, 9);
-console.log("La respuesta de la resta es: ", respuestaResta);
+const respuestaResta = paqueteResta.resta(3, 2);
+console.log("La respuesta de la resta es: ", respuestaResta); // 1
 
 const paqueteMultiplicacion = require("2025a-swgr1-djga-multiplicacion");
-const respuestaMultiplicacion = paqueteMultiplicacion.multiplicacion(5, 4);
-console.log("La respuesta de la multiplicación es: ", respuestaMultiplicacion);
+const respuestaMultiplicacion = paqueteMultiplicacion.multiplicacion(2, 5);
+console.log("La respuesta de la multiplicación es: ", respuestaMultiplicacion); // 10
 
 const paqueteDivision = require("2025a-swgr1-djga-division");
-const respuestaDivision = paqueteDivision.multiplicacion(20, 5);
+const respuestaDivision = paqueteDivision.multiplicacion(9, 3); // 3
 console.log("La respuesta de la división es: ", respuestaDivision);
