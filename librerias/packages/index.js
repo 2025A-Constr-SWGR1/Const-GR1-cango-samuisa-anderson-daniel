7417851a0dbded59d03e8cb488241941eec24975
AndersonCango <118143904+AndersonCango@users.noreply.github.com>
2025-07-31 20:47:04 -0500

    feat: add own library

--- librerias/packages/index.js
+++ librerias/packages/index.js
@@ -5,3 +5,11 @@ console.log("La respuesta de la suma es: ", respuestaSuma);
 const paqueteResta = require("2025a-swgr1-adcs-resta");
 const respuestaResta = paqueteResta.resta(24, 9);
 console.log("La respuesta de la resta es: ", respuestaResta);
+
+const paqueteMultiplicacion = require("2025a-swgr1-djga-multiplicacion");
+const respuestaMultiplicacion = paqueteMultiplicacion.multiplicacion(5, 4);
+console.log("La respuesta de la multiplicación es: ", respuestaMultiplicacion);
+
+const paqueteDivision = require("2025a-swgr1-djga-division");
+const respuestaDivision = paqueteDivision.multiplicacion(20, 5);
+console.log("La respuesta de la división es: ", respuestaDivision);
